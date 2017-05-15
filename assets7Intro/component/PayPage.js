b05d9d0f1d26987e40a1e92b39e0f31356adfa4d
royding <cstdingran@gmail.com>
2017-05-15 10:47:30 +0800

    fix

--- assets7Intro/component/PayPage.js
+++ assets7Intro/component/PayPage.js
@@ -21,8 +21,6 @@ const Timeout = require('./Timeout');
 const Modal = require('./Modal');
 const FirstSharePanel = require('./FirstSharePanel');
 
-var REMAIN_NUM = 1000;
-
 var PayPage = React.createClass({
 
     getInitialState(){
@@ -35,7 +33,7 @@ var PayPage = React.createClass({
             QQLink: null, //QQ群链接
             QQCode: null, //QQ暗号
             showBackup: false, //显示备用QQ
-            remain: parseInt(localStorage.getItem('remain-num'))||REMAIN_NUM, //剩余席位
+            remain: parseInt(localStorage.getItem('remain-num')) || Util.getUserNumber(), //剩余席位
 
             //21days2.0
             hasSenior: false, //是否有上线
@@ -329,26 +327,9 @@ var PayPage = React.createClass({
                 {this.state.showBackup && <a className="backup-text" href="http://jq.qq.com/?_wv=1027&k=41976jN">QQ群号：
                     <span className="red-text">429827363</span>
                     <p className="red-text  tada animated infinite">暗号：7天</p></a>}
-                {/*如果未报名，并且不是下线，则显示报名时间和人数*/}
-                {!this.state.hasPaid &&
-                <div>
-                   {!this.state.hasSenior && <div>
-                     <div className="top-time-bottom">
-                         <div className="top-time">
-                             <Timeout hasEnded={this.state.time} finalDate={this.state.endTime}/>
-                         </div>
-                     <div className="entered">
-                         <div className="show-entered">
-                             <img src="./assets7Intro/image/number.png" />
-                             <div className="show-number"> 剩余名额</div>
-                         </div>
-                         {/*<span>0</span>*/}
-                         {this.state.showint ? <span>{this.state.num}</span>:<span>0</span>}
-                         </div>
-                     </div>
-                   </div>}
 
-                    <img src="./assets7Intro/image/campaign.jpg" className="intro-img"/></div> }
+                {!this.state.hasPaid && <img src="./assets7Intro/image/bg_new.png" className="intro-img"/>}
+
                 {/*如果已经报名，报名链接时展示*/}
                 {this.state.hasPaid && <div>
                     <div className="paid-bg" style={{height:window.innerHeight}}>
@@ -391,6 +372,20 @@ var PayPage = React.createClass({
 
                 {!this.state.hasPaid &&
                     <div className="bottom-button">
+                        {/*如果未报名，并且不是下线，则显示报名时间和人数*/}
+                        {!this.state.hasSenior && <div>
+                          <div className="top-time-bottom">
+                              <div className="top-time">
+                                  <Timeout hasEnded={this.state.time} finalDate={this.state.endTime}/>
+                              </div>
+                              <div className="entered">
+                                  <div className="show-entered">
+                                      <div className="show-number"> 剩余名额</div>
+                                  </div>
+                                  {this.state.showint ? <span>{this.state.num}</span>:<span>0</span>}
+                              </div>
+                          </div>
+                        </div>}
                         {((this.state.time || !this.state.showint ) && !this.state.hasSenior) ? <span onClick={this.didClickHandler}  className="join-button">还想报名？点我！</span> : <span onClick={this.clickHandler}  className={!this.state.hasSenior ?"join-button":"whole-join-button"}>立即参加（￥{this.state.buttonPrice}）</span>}
                         <span className="share-button" onClick={this.shareModalHandler}>邀请好友</span>
                     </div>
