133059baf610579eda9c33a45c246ab2ab5d295e
FocusOnWrittingBug <506792446@qq.com>
2017-06-13 09:01:02 +0800

    2017.6.12 笔记卡

---
+++ assetsFund/CourseInfo.js
@@ -0,0 +1,82 @@
+/**
+ * Created by zhanheng on 17/6/12.
+ */
+const courseInfo = [
+    {
+        id: 10,
+        title: '基金课0',
+        cardTitle: '学前班'
+    },
+    {
+        id: 11,
+        title: '基金课1',
+        cardTitle: '学前班'
+    },
+    {
+        id: 12,
+        title: '基金课2',
+        cardTitle: '学前班'
+    },
+    {
+        id: 13,
+        title: '基金课3',
+        cardTitle: '学前班'
+    },
+    {
+        id: 14,
+        title: '基金课4',
+        cardTitle: '学前班'
+    },
+    {
+        id: 15,
+        title: '基金课5',
+        cardTitle: '学前班'
+    },
+    {
+        id: 16,
+        title: '基金课6',
+        cardTitle: '学前班'
+    },
+    {
+        id: 17,
+        title: '基金课7',
+        cardTitle: '学前班'
+    },
+    {
+        id: 18,
+        title: '基金课8',
+        cardTitle: '学前班'
+    },
+    {
+        id: 19,
+        title: '基金课9',
+        cardTitle: '学前班'
+    },
+    {
+        id: 20,
+        title: '基金课10',
+        cardTitle: '学前班'
+    },
+    {
+        id: 21,
+        title: '基金课11',
+        cardTitle: '学前班'
+    },
+    {
+        id: 22,
+        title: '基金课12',
+        cardTitle: '学前班'
+    },
+    {
+        id: 23,
+        title: '基金课13',
+        cardTitle: '学前班'
+    },
+    {
+        id: 24,
+        title: '基金课14',
+        cardTitle: '学前班'
+    },
+]
+
+module.exports = courseInfo
