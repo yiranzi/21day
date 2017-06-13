4cee779e11cb9189b8edb84c55afb22c84a26b9b
veronica <87801036@qq.com>
2017-06-13 15:29:00 +0800

    报名页样式

--- assetsFund/Util.js
+++ assetsFund/Util.js
@@ -47,8 +47,8 @@ const CURRENT_BATCH = 2; //当前期数
 const END_TIME = [2017,6,27,0,0,0]; // TODO roy 截止时间，需要和后台同步
 const USER_NUMBER = 500; // TODO roy 活动报名总人数
 
-const NORMAL_PRICE = 580;// TODO roy 上线价格，普通用户
-const CHEAP_PRICE = 580;// TODO roy 下线价格，享有的优惠价格
+const NORMAL_PRICE = 1;// TODO roy 上线价格，普通用户
+const CHEAP_PRICE = 1;// TODO roy 下线价格，享有的优惠价格
 
 const SHARE_TITLE = '和我一起提高财商吧！';
 const SHARE_DESC = '邀请你一起参加7天财商训练营';
