1b8c3bd75991e233c16841fc11ae23f1f512c56c
sunyiran <570932296@qq.com>
2017-07-12 10:25:25 +0800

    修改bug

--- assetsPlus/view/page/HomeCourseList.js
+++ assetsPlus/view/page/HomeCourseList.js
@@ -102,7 +102,7 @@ const HomeCourseList = React.createClass({
         Loading.showLoading('获取课程中...');
         let enterWhere;
 
-        Material.postData('pai课程点击' + index);
+        Material.postData('pai课程点击' + courseId);
         Tools.fireRaceCourse(courseId).then((value)=>{
             //0保存上当前的课程ID
             if (value === 'pay') {
